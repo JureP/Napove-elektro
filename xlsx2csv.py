a97575f966972197e50365c51544823a20bce4fa
Jure Podlogar <jure.podlogar@gmail.com>
2015-10-22 21:28:50 +0200

    Sprememba imena csv, dodana datum in ura v csv vrstico

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -2,6 +2,7 @@ import os
 import xlrd
 import csv
 import re
+import datetime
 
 ## okolje v katerem so podatki, in kamor se shranijo csv-ji
 Okolje = 'C:/Users/Podlogar/Documents/Projekt Elektro/01_Priprava podatkov/Podatki/RNF - Meritve in ARSO podatki'
@@ -13,7 +14,7 @@ for exl in os.listdir(Okolje):
         vhodnoIme = exl
         kraj = re.search('(?<=ARSO-)(.*)(?=.xlsx)', vhodnoIme)
         ## ime ustvarjenega csv-ja
-        imeCSV = kraj.group() + '_vreme.csv'
+        imeCSV = 'vreme_' + kraj.group() + '.csv'
         print(imeCSV)
         ## branje xlsx-a
         with xlrd.open_workbook(vhodnoIme) as beri:
@@ -22,6 +23,14 @@ for exl in os.listdir(Okolje):
             ## pisanje csv-ja
             with open(imeCSV, 'w', newline='') as csvfile:
                 wr = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                ## zapise samo napoved vremena (z title) (DODAK SE DATUM IN URO!!)
-                for rownum in range(1,beriStran.nrows):
-                    wr.writerow(beriStran.row_values(rownum)[2:9])
+                ## zapis naslovov vrstic
+                wr.writerow(beriStran.row_values(1)[0:9])
+                ## zapise samo napoved vremena
+                for rownum in range(2,beriStran.nrows):
+                    ## datum in ura napovedi
+                    datumUra = xlrd.xldate_as_tuple(beriStran.row_values(rownum)[0], beri.datemode)
+                    vrstica = [datetime.date(*datumUra[0:3]), datetime.time(*datumUra[3:6])] + beriStran.row_values(rownum)[2:9]
+                    wr.writerow(vrstica)
+
+
+
