9adb98286c373dceaba86a43dbd661ac83b0f5ae
Jure Podlogar <jure.podlogar@gmail.com>
2015-11-07 19:41:27 +0100

    spremembe opisa

--- Funkcije/razdelitevPodatkov_funkcija.r
+++ Funkcije/razdelitevPodatkov_funkcija.r
@@ -1,20 +1,23 @@
 razdelitevNapovedi <- function(
-	## vrne seznam razdeljenih napovedi vremenskeih parametrov.
+	## vrne seznam razdeljenih napovedi vremenskeih parametrov (oziroma feature matriko).
 	## podatki so razdeljeni na train in test del (naceloma 2 
 	## 2 leti za train, 1 leto za test)
 	vremenskaNapoved, ## xts matrika napovedi vremenskih parametrov
 	zacetek = as.POSIXct('2012-01-01 00:00:00',format='%Y-%m-%d %H:%M:%S'), ## zacetek train mnozice
 	konecTrain = as.POSIXct('2014-01-01 00:00:00',format='%Y-%m-%d %H:%M:%S'), ## kone train mnozice
+	konecValid = as.POSIXct('2014-06-01 00:00:00',format='%Y-%m-%d %H:%M:%S'), ## konec valid mnozice
 	konecTest = as.POSIXct('2014-12-31 23:00:00',format='%Y-%m-%d %H:%M:%S') ## konec testne mnozice
 	){
 	
-	xTrain <- vremenskaNapoved[index(vremenskaNapoved) >= zacetek 
+		xTrain <- vremenskaNapoved[index(vremenskaNapoved) >= zacetek 
 						& index(vremenskaNapoved)  < konecTrain, ]
-	xTest <- vremenskaNapoved[index(vremenskaNapoved) >= konecTrain 
+		xValid <- vremenskaNapoved[index(vremenskaNapoved) >= konecTrain 
+						& index(vremenskaNapoved)  < konecValid, ]
+		xTest <- vremenskaNapoved[index(vremenskaNapoved) >= konecValid 
 						& index(vremenskaNapoved)  <= konecTest, ]
 	
-	loceni <- list('xTrain' = xTrain, 'xTest' = xTest)
-	return(loceni)
+		loceni <- list('xTrain' = xTrain, 'xValid' = xValid, 'xTest' = xTest)
+		return(loceni)
 	}
 	
 	
@@ -25,13 +28,16 @@ razdelitevRealizacije <- function(
 	realizacija, ## xts matrika realizacije proizvodnje
 	zacetek = as.POSIXct('2012-01-01 00:00:00',format='%Y-%m-%d %H:%M:%S'), ## zacetek train mnozice
 	konecTrain = as.POSIXct('2014-01-01 00:00:00',format='%Y-%m-%d %H:%M:%S'), ## kone train mnozice
+	konecValid = as.POSIXct('2014-06-01 00:00:00',format='%Y-%m-%d %H:%M:%S'), ## konec valid mnozice
 	konecTest = as.POSIXct('2014-12-31 23:00:00',format='%Y-%m-%d %H:%M:%S') ## konec testne mnozice
 	){
 	
-	yTrain <- realizacija[index(realizacija) >= zacetek 
+		yTrain <- realizacija[index(realizacija) >= zacetek 
 						& index(realizacija)  < konecTrain, ]
-	yTest <- realizacija[index(realizacija) >= konecTrain 
+		yValid <- realizacija[index(realizacija) >= konecTrain 
+						& index(realizacija)  < konecValid, ]
+		yTest <- realizacija[index(realizacija) >= konecValid 
 						& index(realizacija)  <= konecTest, ]
-	loceni <- list('yTrain' = yTrain, 'yTest' = yTest)
-	return(loceni)
+		loceni <- list('yTrain' = yTrain, 'yValid' = yValid, 'yTest' = yTest)
+		return(loceni)
 	}
